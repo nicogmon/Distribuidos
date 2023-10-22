b9ce1d40974b7e19f939bc6954b62fc3d781cd98
Nicolas <n.garciam.2021@alumnos.urjc.es>
2023-10-22 12:53:38 +0200

    ulitmas modificaciones estilo

--- P2/stub.c
+++ P2/stub.c
@@ -17,6 +17,11 @@
 
 
 #define MAX_CLIENTS 2 
+#define PID_LENGTH 5
+#define MAX_CONNECTIONS 2
+#define FALSE 0
+#define TRUE 1
+
 
 unsigned int clock_lamport = 0;
 
@@ -24,33 +29,31 @@ int tcp_socket;
 int tcp_socket1;
 int tcp_socket3;
 
-int tcp_sockets [2];
+int tcp_sockets [MAX_CLIENTS];
 
-char Pid [5];
+char Pid [PID_LENGTH];
 int i = 0;
 
 pthread_t recv_thread;
 
-int init(int flag, char * ip, long port, const char * id){
+int init(int flag, char * ip, long port, const char * id) {
     strcpy(Pid, id);
-    //printf("Pid %s\n", Pid);
     if (flag == 1){
         init_Server(port);
-    }
-    else if (flag == 2){
+    } else if (flag == 2){
         init_Client(ip, port);
     }
     return 0;
 }
 
-int init_Server(long port){
+int init_Server(long port) {
     
     setbuf(stdout, NULL);
     
     struct sockaddr_in server_addr;
 
     server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);	//Meter ips si no no funciona desde otra maquina 
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	server_addr.sin_port = htons(port);
 
     int addrlen = sizeof(server_addr);
@@ -60,7 +63,6 @@ int init_Server(long port){
         exit(EXIT_FAILURE);
     }
     
-
     const int enable = 1;
     
     if (setsockopt
@@ -75,8 +77,7 @@ int init_Server(long port){
 		exit(EXIT_FAILURE);
 	}
     
-
-    if (listen(tcp_socket, 5) < 0) {
+    if (listen(tcp_socket, MAX_CONNECTIONS) < 0) {
 		perror("listen");
 		exit(EXIT_FAILURE);
 	}
@@ -85,7 +86,6 @@ int init_Server(long port){
     threadArgs  * args = (threadArgs *) malloc(sizeof(threadArgs));
     args->server_addr = &server_addr;
     args->addrlen = &addrlen;
-
     
     pthread_create(&thread_id, NULL, accept_connections, (void *) args);
     
@@ -93,8 +93,8 @@ int init_Server(long port){
     }
 
 // establish connections between processes
-void * accept_connections(void * args){
-    pthread_t thread_ids[2];
+void * accept_connections(void * args) {
+    pthread_t thread_ids[MAX_CLIENTS];
     
 
     threadArgs * arguments =(threadArgs *) args;
@@ -109,22 +109,21 @@ void * accept_connections(void * args){
         }
         pthread_create(&thread_ids[i], NULL, server_receive,NULL);
     }
+
     free(args);
+
     for (int i = 0; i < MAX_CLIENTS; i++){
-        //fprintf(stderr, "thread id %ld\n", thread_ids[i]);
         pthread_join(thread_ids[i], NULL);
     }
-    
     
     return 0;  
 }
 
-void * server_receive(void *arg)
-{   
-    int first = 0;
-    int flag = 1;
+void * server_receive(void *arg) {   
+    int first = TRUE;
+    int flag = TRUE;
     int socket_local = tcp_sockets[i];
-    i = i + 1;
+    i++;
 
     while(flag){
         message * msg = malloc(sizeof(message));
@@ -134,17 +133,16 @@ void * server_receive(void *arg)
                                         msg->origin, get_action(msg->action));
 	    }
         if (msg->action == SHUTDOWN_ACK){
-            flag = 0;
-            
+            flag = FALSE;
         }
-        if (first == 0){
-            if (strcmp(msg->origin, "P1") == 0){
+        if (first){
+            if (strcmp(msg->origin, "P1") == 0) {
                 tcp_socket1 = socket_local;
             }
-            else if (strcmp(msg->origin, "P3") == 0){
+            else if (strcmp(msg->origin, "P3") == 0) { 
                 tcp_socket3 = socket_local;
             }
-            first = 1;
+            first = FALSE;
         }
         
         free(msg);
@@ -154,16 +152,13 @@ void * server_receive(void *arg)
    
 }
 
-
-// receive a message from a remote process
-
-
-int init_Client(char * ip, long port){
+int init_Client(char * ip, long port) {
     
-    if ((tcp_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+    if ((tcp_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("Error creating socket");
         exit(EXIT_FAILURE);
     }
+
     struct in_addr ipv4Addr;
 
     if (inet_pton(AF_INET, ip, &ipv4Addr) <= 0) {
@@ -178,7 +173,7 @@ int init_Client(char * ip, long port){
 
     int addrlen = sizeof(server_addr);
 
-    if(connect(tcp_socket, (struct sockaddr *) &server_addr, addrlen) < 0) {
+    if (connect(tcp_socket, (struct sockaddr *) &server_addr, addrlen) < 0) {
         perror("connect");
         exit(EXIT_FAILURE);
     }
@@ -186,15 +181,14 @@ int init_Client(char * ip, long port){
     return tcp_socket;
 }
 
-int waiting_order()
-{
+int waiting_order() {
     pthread_t thread_id;
     pthread_create(&thread_id, NULL, receive_messages,NULL);
 	pthread_join(thread_id, NULL);
 	return 0;
 }
 
-void * receive_messages(){
+void * receive_messages() {
     message * msg = malloc(sizeof(message));
 	
 	if (recv(tcp_socket, msg, sizeof(message), 0) > 0) {
@@ -206,10 +200,9 @@ void * receive_messages(){
     free(msg);
 	return NULL;
     
-    return 0;
 }
 
-int ready_to_shutdown(){
+int ready_to_shutdown() {
     message msg_out = {0};
     msg_out.action = READY_TO_SHUTDOWN;
     update_clock(NULL, &msg_out);
@@ -220,12 +213,11 @@ int ready_to_shutdown(){
     return 0;
 }
 
-int shutdown_now(int dest){
+int shutdown_now(int dest) {
     
     message msg_out = {0};
     msg_out.action = SHUTDOWN_NOW;
     update_clock(NULL, &msg_out);
-    //msg_out.clock_lamport = get_clock_lamport();
     strcpy(msg_out.origin,Pid);
 
     send_message(&msg_out, dest);
@@ -233,26 +225,24 @@ int shutdown_now(int dest){
     return 0;
 }
 
-int shutdown_ack(){
+int shutdown_ack() {
     
     message msg_out = {0};
     msg_out.action = SHUTDOWN_ACK;
     update_clock(NULL, &msg_out);
-    //msg_out.clock_lamport = get_clock_lamport();
     strcpy(msg_out.origin,Pid);
 
     send_message(&msg_out, 0);
+
     return 0;
 }
 
-// send a message to a remote process
-int send_message(message * msg, int arg_socket){
+int send_message(message * msg, int arg_socket) {
     
-    if (arg_socket != 0){
-        if (arg_socket == 1){
+    if (arg_socket != 0) {
+        if (arg_socket == 1) {
             tcp_socket = tcp_socket1;
-        }
-        else if (arg_socket == 3){
+        } else if (arg_socket == 3) {
             tcp_socket = tcp_socket3;
         }
     }
@@ -265,44 +255,39 @@ int send_message(message * msg, int arg_socket){
 }
 
 // update Lamport clock value
-void update_clock(message* msg_in, message* msg_out){
+void update_clock(message* msg_in, message* msg_out) {
 
-    if (msg_out != NULL){
+    if (msg_out != NULL) {
         clock_lamport++;
         msg_out->clock_lamport = clock_lamport;
-    }
-    else {
+    } else {
         clock_lamport = get_max(clock_lamport, msg_in->clock_lamport) + 1 ;
     }
 }
 
-unsigned int get_clock_lamport(){
+unsigned int get_clock_lamport() {
     return clock_lamport;
 }
 // get current Lamport clock value
 
 
-unsigned int get_max(unsigned int a, unsigned int b){
+unsigned int get_max(unsigned int a, unsigned int b) {
     if (a > b){
         return a;
-    }
-    else {
+    } else {
         return b;
     }
 }
 
 
 char * get_action(int action){
-    if (action == READY_TO_SHUTDOWN){
+    if (action == READY_TO_SHUTDOWN) {
         return "READY_TO_SHUTDOWN";
-    }
-    else if (action == SHUTDOWN_NOW){
+    } else if (action == SHUTDOWN_NOW) {
         return "SHUTDOWN_NOW";
-    }
-    else if (action == SHUTDOWN_ACK){
+    } else if (action == SHUTDOWN_ACK) {
         return "SHUTDOWN_ACK";
-    }
-    else {
+    } else {
         return "ERROR";
     }
 }
