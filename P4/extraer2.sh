746a02be007e29fa34e5e44f4d6c6853b576aee4
Nicolas <n.garciam.2021@alumnos.urjc.es>
2023-12-19 18:13:04 +0100

    version para sacr medidas con herramientoas para extraer latencias

--- P4/extraer2.sh
+++ P4/extraer2.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
 # Encuentra todos los archivos que coinciden con el patrón "E2_0/info*txt"
-files=(E3_0/info*txt)
+folders=( $(find . -maxdepth 1 -type d -name 'E*_?' | sort) )
+
+for folder in "${folders[@]}"; do
+    echo "Carpeta: $folder"
+    files=(E3_0/info*txt)
 
 
-for file in "${files[@]}"; do
-    echo $file
-    identifier=$(echo "$file" | grep -oP 'info(\d+)' | grep -oP '\d+')
-    echo $identifier
-    if [ -e "E1_0/latencia$identifier" ]; then
-        rm "E1_0/latencia$identifier"
-    fi
-    
-    # Obtiene solo los valores de latencia y los imprime
-    grep -oP 'Latencia: \K[\d.]+' "$file" >> "E3_0/latencia$identifier"
-    
+    for file in "${files[@]}"; do
+        echo $file
+        identifier=$(echo "$file" | grep -oP 'info(\d+)' | grep -oP '\d+')
+        echo $identifier
+        if [ -e "E1_0/latencia$identifier" ]; then
+            rm "E1_0/latencia$identifier"
+        fi
+        
+        # Obtiene solo los valores de latencia y los imprime
+        grep -oP 'Latencia: \K[\d.]+' "$file" >> "E3_0/latencia$identifier"
+        
+    done
 done
